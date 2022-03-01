4d3046ffca934413d00020f9dbce11d8b558d3a6
Nikita Kozlov <nikita@thetourguy.com>
2022-03-01 18:20:20 +0300

    abstract

--- Lab1/Constants/Ingredients/Menu.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab1.Constants.Ingredients;
-
-public static class Menu
-{
-    public static BreadTypes[] BreadTypes => Enum.GetValues<BreadTypes>();
-    public static SauceTypes[] SauceTypes => Enum.GetValues<SauceTypes>();
-    public static VegetablesTypes[] VegetablesTypes => Enum.GetValues<VegetablesTypes>();
-    public static СutletTypes[] СutletTypes => Enum.GetValues<СutletTypes>();
-}
